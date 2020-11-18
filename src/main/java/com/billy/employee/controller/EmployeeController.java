43393f9bc1adcd4a8c210df368901df34f01666f
BillyJ <bisohhj@gmail.com>
2020-11-18 17:22:22 -0500

    idea merge to github test

---
+++ src/main/java/com/billy/employee/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.billy.employee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description: <br>
+ *
+ * @author: Billy <br>
+ * @createDate: 2020-11-18 <br>
+ */
+
+@RestController
+@RequestMapping("/spring-rest")
+public class EmployeeController {
+
+
+}
