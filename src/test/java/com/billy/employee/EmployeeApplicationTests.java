f044ac7a00a9db2718a58f459768cb1521b4e0db
BillyJ <bisohhj@gmail.com>
2020-11-18 16:59:04 -0500

    idea merge to github test

---
+++ src/test/java/com/billy/employee/EmployeeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.billy.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class EmployeeApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
